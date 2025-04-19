014c1da6b1da3e810340e439cd1006827a8a0c58
Nathália Ficher <nathaliaficher.dev@outlook.com>
2025-04-18 21:16:46 -0300

    Compilado de exercicios 3 em 1

---
+++ TESTES/Ex-6/Ex6.js
@@ -0,0 +1,19 @@
+let numero = Number(prompt("Digite um numero:"))
+
+for(let i = 0; i <= numero; i++){
+    console.log(i)
+} 
+
+console.log("Fim simples")
+
+for(let cinco = 0; cinco <= 50; cinco+=5){
+    console.log(cinco)
+}
+
+console.log("Fim 50+")
+
+for(let menosc = 50; menosc >= 0; menosc-=5){
+    console.log(menosc)
+}
+
+console.log("Fim 50-")
