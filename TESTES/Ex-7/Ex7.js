99eaa0d24e76a8ad937002409252b1d584c145bd
Nathália Ficher <nathaliaficher.dev@outlook.com>
2025-04-20 18:33:47 -0300

    Exercicio 7 com erro/pesquisar e resolver

---
+++ TESTES/Ex-7/Ex7.js
@@ -0,0 +1,7 @@
+let n = 5
+for(let tab = n; tab <= n + 2; tab++){
+    console.log("tabuada: " + n)
+    for(let n2 = 0; n2 <= 10; n2++){
+        console.log(n + " x " + n2 + " = " + (n*n2))
+    }
+}
