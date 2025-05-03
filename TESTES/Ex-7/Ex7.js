2bf801847856da09246118a3f1d79878c4a8e0cf
Nathália Ficher <nathaliaficher.dev@outlook.com>
2025-05-03 19:52:51 -0300

    Exercicio 7 resolvido o erro

--- TESTES/Ex-7/Ex7.js
+++ TESTES/Ex-7/Ex7.js
@@ -1,7 +1,8 @@
-let n = 5
+let n = Number(prompt('Digite um numero: '))
+
 for(let tab = n; tab <= n + 2; tab++){
-    console.log("tabuada: " + n)
+    console.log("tabuada: " + tab)
     for(let n2 = 0; n2 <= 10; n2++){
-        console.log(n + " x " + n2 + " = " + (n*n2))
+        console.log(tab + " x " + n2 + " = " + (n*n2))
     }
 }
